9892b63e3eb40f65bd550284dd3c5342864128ed
shrujandev <shrujandev1824@gmail.com>
2022-03-16 21:17:52 +0530

    menu updates

--- client.py
+++ client.py
@@ -19,6 +19,27 @@ def simple():
     return str(princ)+','+str(time)+','+str(rate)
 
 
+    
+def numericalCal():
+    run = True
+    while run:
+        choice  = input("1.Addition\n2.Substraction\n3.Multiplication\n4.Division\n5.Main Menu\nEnter Your Choice :")
+        if choice == "1":
+            data=inp()+"1"
+        elif choice == "2":
+            data = inp()+"2"
+        elif choice == "3":
+            data = inp()+"3"
+        elif choice == "4":
+            data = inp()+"4"
+        else:
+            run = False
+            print("Exit Initiated")
+            clientSocket.close()
+            exit(0)
+    
+    
+
 print("Enter Server Details\n")
 serverIP =  input("Enter Server IP : ")
 print("Connecting to server ...\n")
@@ -37,9 +58,9 @@ os.system('cls')
 run = True
 
 while run:
-    choice  = input("1.Addition\n2.SUB\n3.MUL\n4.DIV\n5.Simple Interest\nEnter Your Choice :")
+    choice  = input("Welcome to Main Menu\n1.Simple Calculator\n2.Trignometric Calculator\n3.Financial Calculator\n4.Exit\nEnter Your Choice :")
     if choice == "1":
-        data=inp()+"1"
+        data=numericalCal()
     elif choice == "2":
         data = inp()+"2"
     elif choice == "3":
@@ -59,7 +80,3 @@ while run:
     dataFromServer = clientSocket.recv(1024);
     print(dataFromServer.decode());
     clientSocket.close()
-    
-
-    
-    
