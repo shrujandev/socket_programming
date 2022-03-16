2f7194790bf5be0d55724466fed6061c82d3031c
shrujandev <shrujandev1824@gmail.com>
2022-03-16 21:17:32 +0530

    menu updates

--- client.py
+++ client.py
@@ -1,30 +1,43 @@
 
-#----- A simple TCP client program in Python using send() function -----
-
 import socket
 import  os
+import time 
 
 def inp():
-    val1 = input("Enter first Number :")
-    val2 = input("Enter Second Number :")
-    return str(val1)+','+str(val2)+','
+    dataString=''
+    num = int(input("Enter The Number of Elements"))
+    for i in range(num):
+        val = input("Enter Numbers : ")
+        dataString = dataString +','+ str(val)
+    return dataString
 
+def simple():
+    princ = input("Enter Principal Amount")
+    time = input("Enter Time in Years")
+    rate = input("Enter Rate of Interest")
+
+    return str(princ)+','+str(time)+','+str(rate)
 
 
 print("Enter Server Details\n")
 serverIP =  input("Enter Server IP : ")
-os.system('cls')
+print("Connecting to server ...\n")
 
- 
+
+
+
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+clientSocket.connect((serverIP,12000))
+print("Connection Established\n")
+time.sleep(3)
+os.system('cls')
 
 # Connect to the server
 
 run = True
 
 while run:
-    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
-    clientSocket.connect(("192.168.98.76",12000));
-    choice  = input("1.ADD\n2.SUB\n3.MUL\n4.DIV\n5.Simple Interest\nEnter Your Choice :")
+    choice  = input("1.Addition\n2.SUB\n3.MUL\n4.DIV\n5.Simple Interest\nEnter Your Choice :")
     if choice == "1":
         data=inp()+"1"
     elif choice == "2":
@@ -33,8 +46,8 @@ while run:
         data = inp()+"3"
     elif choice == "4":
         data = inp()+"4"
-    #elif choice =="5":
-        #data = simple()+"5"
+    elif choice =="5":
+        data = simple()+"5"
     else:
         run = False
         print("Exit Initiated")
