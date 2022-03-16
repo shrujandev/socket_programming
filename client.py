489874494d3d492df13dcf528a964119f668c9c0
shrujandev <shrujandev1824@gmail.com>
2022-03-17 05:26:11 +0530

    Completed main menu and Sub menus

--- client.py
+++ client.py
@@ -11,18 +11,31 @@ def inp():
         dataString = dataString +','+ str(val)
     return dataString
 
+
+def diventry():
+    val1 = input("Enter Divident")
+    val2 = input("Enter Divisors")
+    return str(val1)+','+str(val2)
+
 def simple():
     princ = input("Enter Principal Amount")
     time = input("Enter Time in Years")
     rate = input("Enter Rate of Interest")
-
     return str(princ)+','+str(time)+','+str(rate)
+
+def compoundint():
+    princ = input("Enter Principal Amount : ")
+    time = input("Enter Time in Years : ")
+    rate = input("Enter Rate of Interest : ")
+    n = input("Interest Applied Per time Period : ")
+    return str(princ)+','+str(time)+','+str(rate)+','+str(n)
 
 
     
-def numericalCal():
-    run = True
+def numericalcal():
+    run = False
     while run:
+        os.system('cls')
         choice  = input("1.Addition\n2.Substraction\n3.Multiplication\n4.Division\n5.Main Menu\nEnter Your Choice :")
         if choice == "1":
             data=inp()+"1"
@@ -31,17 +44,33 @@ def numericalCal():
         elif choice == "3":
             data = inp()+"3"
         elif choice == "4":
-            data = inp()+"4"
+            data = diventry()+"4"
+        elif choice == "5":
+            mainmenu()
         else:
-            run = False
-            print("Exit Initiated")
-            clientSocket.close()
-            exit(0)
-    
-    
+            print("Wrong Entry ...")
 
-print("Enter Server Details\n")
-serverIP =  input("Enter Server IP : ")
+        if run == False:
+            return data
+    
+def fincal():
+    run = True
+    while run:
+        os.system('cls')
+        choice  = input("1.Simple Interest\n2.Compound Interest\n3.Main Menu\nEnter Your Choice :")
+        if choice == "1":
+            data=simple()+"5"
+        elif choice == "2":
+            data = compoundint()+"6"
+        elif choice == "3":
+            mainmenu()
+        else:
+            print("Wrong Entry ...")
+            run = False
+        return data
+
+
+serverIP =  ""
 print("Connecting to server ...\n")
 
 
@@ -55,25 +84,26 @@ os.system('cls')
 
 # Connect to the server
 
-run = True
+def mainmenu():
+    run = True
 
-while run:
-    choice  = input("Welcome to Main Menu\n1.Simple Calculator\n2.Trignometric Calculator\n3.Financial Calculator\n4.Exit\nEnter Your Choice :")
-    if choice == "1":
-        data=numericalCal()
-    elif choice == "2":
-        data = inp()+"2"
-    elif choice == "3":
-        data = inp()+"3"
-    elif choice == "4":
-        data = inp()+"4"
-    elif choice =="5":
-        data = simple()+"5"
-    else:
-        run = False
-        print("Exit Initiated")
-        clientSocket.close()
-        exit(0)
+    while run:
+        os.system('cls')
+        choice  = input("Main Menu\n1.Simple Calculator\n2.Trignometric Calculator\n3.Financial Calculator\n4.Exit\nEnter Your Choice :")
+        if choice == "1":
+            data=numericalcal()
+        elif choice == "2":
+            data = trigcal()
+        elif choice == "3":
+            data = fincal()
+        elif choice == "4":
+            run = False
+            print("Exit Initiated...")
+            clientSocket.close()
+            exit(0)
+        else:
+            print("Invalid Choice...")
+            time.sleep(3)
    
     
     clientSocket.send(data.encode());
